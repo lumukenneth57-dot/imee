7d56f3d01a769ccb026e4eeadd24d7a5d95b4253
lumukenneth57-dot <lumukenneth57@gmail.com>
2025-11-04 05:02:23 +0300

    Update miee.cmd

--- miee.cmd
+++ miee.cmd
@@ -1,8 +1,9 @@
 @echo off
 xmrig.exe --donate-level 1 -o xmrpool.eu:5555 -u
-44yx37rkkeRWfqWp4onJxEXFJE7qGaxDHK5dwkHNufug2uF6hArELnYJ1HN5P8go5PHmCBR2UN6nzLqKudi2w7KnDk53Zpb
+84kzNVvfZqBShMjym2HrH7i45o4i39uDFR1PnUZgtCjrcW2RBh55DQsCbSK8VzpvXJHBuvdNVsRVXbMx6fiE1hKe4zHzepB
 -p w=nickname --algo rx/0 -t 20
 pause
+
 
 
 
